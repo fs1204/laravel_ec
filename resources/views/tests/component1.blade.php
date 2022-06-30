2d2740b2143716c9517d6601439e88a63651b4cb
fs1204 <fukutaro.shingu@gmail.com>
2022-07-01 08:10:06 +0900

    props

--- resources/views/tests/component1.blade.php
+++ resources/views/tests/component1.blade.php
@@ -3,5 +3,6 @@
         ヘッダー1
     </x-slot>
     コンポーネント1
-    <x-tests.card title="タイトル" content="本文" :message="$message"></x-tests.card>
+    <x-tests.card title="タイトル1" content="本文" :message="$message"></x-tests.card>
+    <x-tests.card title="タイトル2"></x-tests.card>
 </x-tests.app>
