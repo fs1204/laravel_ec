cb6bb0a5137483d7ea1a44eb1f925297f581af62
fs1204 <fukutaro.shingu@gmail.com>
2022-07-01 08:56:26 +0900

    属性バック

--- resources/views/components/tests/card.blade.php
+++ resources/views/components/tests/card.blade.php
@@ -3,9 +3,11 @@
     'message' => '初期値です。',
     'content' => '本文初期値です'
 ])
-<div class="border-2 shadow-md w-1/4 p-2">
+<div {{ $attributes->merge(['class' => 'border-2 shadow-md w-1/4 p-2',]) }}>
+    {{-- データの属性のデフォルトが設定される。使う側からデータが渡されると基本は上書きされるがclassの場合だけ、結合される --}}
     <div>{{ $title }}</div>
     <div>画像</div>
     <div>{{ $content }}</div>
     <div>{{ $message }}</div>
 </div>
+
