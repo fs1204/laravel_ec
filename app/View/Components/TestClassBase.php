6662bf093b727862aefb2209dda87a9f66135c13
fs1204 <fukutaro.shingu@gmail.com>
2022-07-01 11:06:21 +0900

    classBase

---
+++ app/View/Components/TestClassBase.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\View\Components;
+
+use Illuminate\View\Component;
+
+class TestClassBase extends Component
+{
+    /**
+     * Create a new component instance.
+     *
+     * @return void
+     */
+    public function __construct(public $classBaseMessage, public $defaultMessage = "初期値です")
+    {
+
+        $this->classBaseMessage = $classBaseMessage;
+        $this->defaultMessage = $defaultMessage;
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\Contracts\View\View|\Closure|string
+     */
+    public function render()
+    {
+        return view('components.tests.test-class-base-component');
+    }
+}
