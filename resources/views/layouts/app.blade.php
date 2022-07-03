3648e203f4f58ae405305834955b4cf4209fd166
fs1204 <fukutaro.shingu@gmail.com>
2022-07-03 17:46:31 +0900

    リソースコントローラ

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,7 +25,6 @@
                 @include('layouts.user-navigation')
             @endif
 
-
             <!-- Page Heading -->
             <header class="bg-white shadow">
                 <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
