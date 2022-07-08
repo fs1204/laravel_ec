17e88045eb926e299551096a56f4bb203b7affba
fs1204 <fukutaro.shingu@gmail.com>
2022-07-09 03:46:38 +0900

    Shop追加

--- app/Models/Owner.php
+++ app/Models/Owner.php
@@ -42,4 +42,8 @@ class Owner extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function shop() {
+        return $this->hasOne(Shop::class);
+    }
 }
