df33861dc2f9def109aca5306019efbaecd86667
fs1204 <fukutaro.shingu@gmail.com>
2022-07-13 08:08:06 +0900

    product index, eager loading

--- app/Http/Controllers/Owner/ProductController.php
+++ app/Http/Controllers/Owner/ProductController.php
@@ -3,10 +3,32 @@
 namespace App\Http\Controllers\Owner;
 
 use App\Http\Controllers\Controller;
+use App\Models\Owner;
+use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ProductController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth:owners');
+
+        $this->middleware(function ($request, $next) {
+
+            $id = $request->route()->parameter('product');
+            if (!is_null($id)) {
+                $productsOwnerId = Product::findOrFail($id)->shop->owner->id;
+                $productId = (int)$productsOwnerId;
+                if ($productId !== Auth::id()) {
+                    abort(404);
+                }
+            }
+
+            return $next($request);
+        });
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -14,7 +36,81 @@ class ProductController extends Controller
      */
     public function index()
     {
-        //
+        // ログインしてるオーナーが所有しているproductを表示したい
+        // $products = Owner::findOrFail(Auth::id())->shop->product; // n+1問題
+
+        // 認証しているownerは1人なので、単数形
+        $ownerInfo = Owner::with('shop.product.imageFirst')     // owner→shop→product→imageFisrt に渡って、まとめて取得する。
+                    ->where('id', Auth::id())           // ログインしているオーナーの情報を取得できる。
+                    ->get();
+
+        // dd($ownerInfo);       Owner, Shop, Product, ImageFirst のモデルをすべて取得している
+        // ^ Illuminate\Database\Eloquent\Collection {#1465 ▼
+        //      #items: array:1 [▼
+        //     0 => App\Models\Owner {#1483 ▼
+        //       ...
+        //       #relations: array:1 [▼
+        //         "shop" => App\Models\Shop {#1477 ▼
+        //           ...
+        //           #relations: array:1 [▼
+        //             "product" => Illuminate\Database\Eloquent\Collection {#1490 ▼
+        //               #items: array:5 [▼
+        //                 0 => App\Models\Product {#1488 ▼
+        //                   ....
+        //                   #relations: array:1 [▼
+        //                     "imageFirst" => App\Models\Image {#1504 ▼
+        //                     }
+        //                   ]
+        //                   ...
+        //                 }
+        //                 1 => App\Models\Product {#1513 ▶}
+        //                 2 => App\Models\Product {#1512 ▶}
+        //                 3 => App\Models\Product {#1511 ▶}
+        //                 4 => App\Models\Product {#1510 ▶}
+        //               ]
+        //               ...
+        //             }
+        //           ]
+        //           ...
+        //         }
+        //       ]
+        //       ....
+        //     }
+        //   ]
+        //   ...
+        // }
+
+        // dd($ownerInfo[0]);  // App\Models\Owner {#1483 ▼
+        // dd($ownerInfo[1]);  // Undefined array key 1
+
+        // view側でfilenameを取得する
+        // foreach($ownerInfo as $owner) {  // $ownerInfo から 値を取り出す 今回は1つ
+                    // dd($owner->shop->product);
+                    // ^ Illuminate\Database\Eloquent\Collection {#1490 ▼
+                    //     #items: array:5 [▼
+                    //       0 => App\Models\Product {#1488 ▶}
+                    //       1 => App\Models\Product {#1513 ▶}
+                    //       2 => App\Models\Product {#1512 ▶}
+                    //       3 => App\Models\Product {#1511 ▶}
+                    //       4 => App\Models\Product {#1510 ▶}
+                    //     ]
+                    //     #escapeWhenCastingToString: false
+                    //   }
+                    // App\Models\Product が ５つあるので、foreach の中にさらに foreach をかける必要がある
+
+            // foreach ($owner->shop->product as $product) {
+            //     echo '<pre>';
+            //     echo $product->imageFirst->filename . PHP_EOL;
+            //     echo '</pre>';
+            //     // sample1.jpg
+            //     // sample2.jpg
+            //     // sample3.jpg
+            //     // sample3.jpg
+            //     // sample4.jpg
+            // }
+        // }
+
+        return view('owner.products.index', compact('ownerInfo'));
     }
 
     /**
