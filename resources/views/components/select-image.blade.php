69fcd8838f06bb4400af63817c13065b82642b99
fs1204 <fukutaro.shingu@gmail.com>
2022-07-14 03:58:51 +0900

    product create

--- resources/views/components/select-image.blade.php
+++ resources/views/components/select-image.blade.php
@@ -3,11 +3,13 @@ if($name === 'image1'){ $modal = 'modal-1'; }
 if($name === 'image2'){ $modal = 'modal-2'; }
 if($name === 'image3'){ $modal = 'modal-3'; }
 if($name === 'image4'){ $modal = 'modal-4'; }
+if($name === 'image5'){ $modal = 'modal-5'; }   // 暫定対応
 @endphp
 
                                 {{-- ↓ (*) --}}
 <div class="modal micromodal-slide" id="{{ $modal }}" aria-hidden="true">
-    <div class="modal__overlay" tabindex="-1" data-micromodal-close>
+    {{-- モーダルが手前に表示されるようにzindexを指定する 指定しないと、「販売中 停止中」がモーダルより前に表示される --}}
+    <div class="modal__overlay z-50" tabindex="-1" data-micromodal-close>
       <div class="modal__container" role="dialog" aria-modal="true" aria-labelledby="{{ $modal }}-title">
         <header class="modal__header">
           <h2 class="text-xl text-gray-700" id="{{ $modal }}-title">
@@ -61,7 +63,8 @@ modalボランとして使うことができる--}}
 {{-- 画像を選んだら、プレジューをしつつ、選んだ画像をコントローラに渡す必要がある
 プレビューエリアとinputタグ(hidden) --}}
 <div class="flex justify-around items-center mb-4">
-    <a data-micromodal-trigger="{{ $modal }}" href='javascript:;'>ファイルを選択</a> {{-- ①より --}}
+    {{-- ボタンっぽくする --}}
+    <a class="py-2 px-4 bg-gray-200" data-micromodal-trigger="{{ $modal }}" href='javascript:;'>ファイルを選択</a> {{-- ①より --}}
     <div class="w-1/4">
         <img id="{{ $name }}_thumbnail" src="">
     </div>
