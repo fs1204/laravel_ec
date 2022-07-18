d52fe2dc5dce0a455d7bfb171b47bdf80dab3868
fs1204 <fukutaro.shingu@gmail.com>
2022-07-18 13:42:34 +0900

    stripe controller

--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -53,4 +53,49 @@ class CartController extends Controller
 
         return redirect()->route('user.cart.index');
     }
+
+    public function checkout()
+    {
+        $user = User::findOrFail(Auth::id());
+        $products = $user->products;
+
+        foreach ($products as $product) { //全てのカートに入っている商品の情報を取り出す
+            // カートに入っている情報はlineItemsとstripeでは呼ばれている。
+            // 商品情報はstripe側に渡すために、stripeで受け取れる形にして送る必要がある。
+            // stripe側で用意しているパラメーターを使う。ドキュメントに書いてある。 https://stripe.com/docs/api/checkout/sessions/create
+            // Stripeに情報を渡すためにセッションでパラメータを渡す。キーとバリューの連想配列の形になっている。
+            // line_itemsというパラメータに設定する必要がある。
+            // line_itemsの中の「show child parameters」をクリック。あらかじめパラーメータが設定されていて、ルールに沿って書いていく。
+            $lineItems[] = [
+                'price' => $product->price,
+                'quantity' => $product->pivot->quantity,
+            ];
+        }
+        // dd($lineItems);
+        // ^ array:2 [▼ カートに商品が入った状態で、 http://localhost/cart/checkout にアクセス。
+        //   0 => array:2 [▼
+        //     "price" => 25621
+        //     "quantity" => 3
+        //   ]
+        //   1 => array:2 [▼
+        //     "price" => 43204
+        //     "quantity" => 6
+        //   ]
+        // ]
+
+        \Stripe\Stripe::setApiKey(env('STRIPE_SECRET_KEY')); // 秘密鍵
+
+        $session = \Stripe\Checkout\Session::create([
+            'line_items' => [$lineItems],
+            'mode' => 'payment',
+            'success_url' => route('user.items.index'), // 支払いが成功したらitems一覧のページにリダイレクトがかかる
+            'cancel_url' => route('cart.cart.index'), // 支払いが失敗したらcartに戻す
+        ]);
+
+        $publicKey = env('STRIPE_PUBLIC_KEY');
+        // 公開鍵
+        // 公開鍵と秘密鍵の2つを組み合わせて決済ができる仕組みになっている。
+
+        return view('user.checkout', compact('session', 'publicKey'));
+    }
 }
