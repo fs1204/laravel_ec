778eea3596f355c117726da6c17bda227a040d40
fs1204 <fukutaro.shingu@gmail.com>
2022-07-19 06:20:30 +0900

    stripe前の在庫処理

--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use App\Models\Cart;
+use App\Models\Stock;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -35,7 +36,7 @@ class CartController extends Controller
             $itemInCart->quantity += $request->quantity; // 数量を追加
             $itemInCart->save();
         } else {
-            Cart::create([ // なければ新規作成
+            Cart::create([  // なければ新規作成
                 'user_id' => Auth::id(),
                 'product_id' => $request->product_id,
                 'quantity' => $request->quantity,
@@ -59,42 +60,45 @@ class CartController extends Controller
         $user = User::findOrFail(Auth::id());
         $products = $user->products;
 
-        foreach ($products as $product) { //全てのカートに入っている商品の情報を取り出す
-            // カートに入っている情報はlineItemsとstripeでは呼ばれている。
-            // 商品情報はstripe側に渡すために、stripeで受け取れる形にして送る必要がある。
-            // stripe側で用意しているパラメーターを使う。ドキュメントに書いてある。 https://stripe.com/docs/api/checkout/sessions/create
-            // Stripeに情報を渡すためにセッションでパラメータを渡す。キーとバリューの連想配列の形になっている。
-            // line_itemsというパラメータに設定する必要がある。
-            // line_itemsの中の「show child parameters」をクリック。あらかじめパラーメータが設定されていて、ルールに沿って書いていく。
-            $lineItems[] = [
-                'price' => $product->price,
-                'quantity' => $product->pivot->quantity,
-            ];
+        foreach ($products as $product) {   // この中でStockテーブルの情報も確認していくことになる
+
+            $quantity = '';
+            $quantity = Stock::where('product_id', $product->id)->sum('quantity');  // 商品の在庫
+
+            // カート内の商品数 > 在庫 なら 変えないので、リダイレクトする。
+            if ($product->pivot->quantity > $quantity) {
+                return redirect()->route('user.cart.index');
+            } else {
+                $lineItems[] = [
+                    'price' => $product->price,
+                    'quantity' => $product->pivot->quantity,
+                ];
+            }
         }
-        // dd($lineItems);
-        // ^ array:2 [▼ カートに商品が入った状態で、 http://localhost/cart/checkout にアクセス。
-        //   0 => array:2 [▼
-        //     "price" => 25621
-        //     "quantity" => 3
-        //   ]
-        //   1 => array:2 [▼
-        //     "price" => 43204
-        //     "quantity" => 6
-        //   ]
-        // ]
+
+        // 全て在庫チェックをして、買える状態にしてから、Stripeに渡す前に、在庫を減らしておく必要がある。
+
+        foreach($products as $product) {
+            Stock::create([
+                'product_id' => $product->id,
+                'type' => \Constant::PRODUCT_LIST['reduce'],    // Constantはファサード
+                'quantity' => $product->pivot->quantity * -1,   // カートの中の在庫数
+            ]);
+        }
+
+        // dd('test'); //マイナスの在庫処理ができているか確認。
+
 
         \Stripe\Stripe::setApiKey(env('STRIPE_SECRET_KEY')); // 秘密鍵
 
         $session = \Stripe\Checkout\Session::create([
             'line_items' => [$lineItems],
             'mode' => 'payment',
-            'success_url' => route('user.items.index'), // 支払いが成功したらitems一覧のページにリダイレクトがかかる
-            'cancel_url' => route('cart.cart.index'), // 支払いが失敗したらcartに戻す
+            'success_url' => route('user.items.index'),
+            'cancel_url' => route('cart.cart.index'),
         ]);
 
         $publicKey = env('STRIPE_PUBLIC_KEY');
-        // 公開鍵
-        // 公開鍵と秘密鍵の2つを組み合わせて決済ができる仕組みになっている。
 
         return view('user.checkout', compact('session', 'publicKey'));
     }
