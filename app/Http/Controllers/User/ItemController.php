e10fdd668765332bd86759f4c19c672abbbb0fe1
fs1204 <fukutaro.shingu@gmail.com>
2022-07-22 22:27:02 +0900

    テストメール

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Mail\TestMail;
 use App\Models\PrimaryCategory;
 use App\Models\Product;
 use App\Models\Stock;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Mail;
 
 class ItemController extends Controller
 {
@@ -29,6 +31,8 @@ class ItemController extends Controller
 
     public function index(Request $request)
     {
+        Mail::to('test@example.com') //受信者の指定
+        ->send(new TestMail());
 
         $categories = PrimaryCategory::with('secondary')->get();
 
